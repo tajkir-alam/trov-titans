31a8de2ea3122051e6acb026dd267af801a3e26f
tajkir-alam <tajkir.alam@gmail.com>
2023-05-18 18:19:32 +0600

    navbar responsive done

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -11,18 +11,28 @@ const Header = () => {
         <li><NavLink to={'/blogs'} className={({ isActive }) => isActive ? "active font-bold" : "hover:bg-transparent"}>Blogs</NavLink></li>
     </>
 
+    const navAccessBtn = () => <>
+        <NavLink to={'/login'} className={({ isActive }) => isActive ? 'active-secondary' : 'common-active-secondary'}>
+            Login
+        </NavLink>
+        <NavLink to={'/registration'} className={({ isActive }) => isActive ? 'active-secondary' : 'common-active-secondary'}>
+            Signup
+        </NavLink>
+    </>
+
     return (
         <div className="navbar custom-container">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <label tabIndex={0} className="btn btn-ghost pr-0 lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         {navItems()}
+                        {navAccessBtn()}
                     </ul>
                 </div>
-                <Link><img src={logo} alt="TrovTitansLogo" className='lg:h-14 btn btn-ghost p-3 md:p-2' /></Link>
+                <Link to={'/'} ><img src={logo} alt="TrovTitansLogo" className='lg:h-14 btn btn-ghost p-3 pl-1 md:p-2' /></Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -30,13 +40,8 @@ const Header = () => {
                 </ul>
             </div>
             <div className="navbar-end gap-2">
-                <div>
-                    <NavLink to={'/login'} className={({ isActive }) => isActive ? 'active-secondary' : 'common-active-secondary'}>
-                        Login
-                    </NavLink>
-                    <NavLink to={'/registration'} className={({ isActive }) => isActive ? 'active-secondary' : 'common-active-secondary'}>
-                        Signup
-                    </NavLink>
+                <div className='flex'>
+                    <span className='hidden md:block'>{navAccessBtn()}</span>
                 </div>
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
