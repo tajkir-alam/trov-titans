7eec47c3e7f0493f0d58fa5979512a5baba26542
tajkir-alam <tajkir.alam@gmail.com>
2023-05-18 11:48:40 +0600

    project routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import img from './assets/slider1.jpg'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <button className="btn">Button</button>
+      <button className="btn">Button</button>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,6 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className='relative'>
+        <img src={img} alt="" className='w-full ' />
+        <h1 className='flex items-center justify-center bg-gradient-to-r w-full h-full top-0  from-[rgba(0,0,0,0.7)] absolute to-[rgba(0,0,0,0.1)] text-white'>fdsijfdsfdsif</h1>
+      </div>
     </>
   )
 }
